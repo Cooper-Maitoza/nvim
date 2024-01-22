019cf24cc6276a70bbd3a489fa93a3ad16f58986
Cooper-Maitoza <coopermaitoza@mail.weber.edu>
2024-01-21 21:35:24 -0700

    01-21-2024 neovim config

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
 require "core"
 
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
 if custom_init_path then
